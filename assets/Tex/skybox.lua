41bf2c02a2b68aff820b1016e8a2cd63e4ee2c63
Cgun-art | michael :) <migueloproio903@gmail.com>
2026-03-01 13:37:20 -0300

    Create Tex/skybox.lua

---
+++ assets/Tex/skybox.lua
@@ -0,0 +1,23 @@
+has DirectX3D
+has DirectX
+has GAMEID
+has sha-recompile
+has sha1-recompile
+has bool
+has Tex
+has Lit6
+has Renderman24
+
+local /GAMEID/
+
+if compile-- true:
+
+[C++]
+[GDK]
+
+create-- object:mesh(cube.8.4.12) Tex=(512x512; color=fill=blue255), NRM=false, Albedo=true, recompile-sha1::true, recompile-sha::true
+
+     end
+
+print("Started. null is false")
+
